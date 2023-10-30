ac016744045bc2cbefa065aad5982f89c21fd5b7
victorzv <kaplantigra@gmail.com>
2023-10-30 21:19:55 +0200

    Test fields

--- CreatePDFFom/Worker.cs
+++ CreatePDFFom/Worker.cs
@@ -1,3 +1,8 @@
+
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+using Aspose.Pdf.Forms;
+using Aspose.Pdf.Text;
 using WorkerService1;
 
 namespace CreatePDFFom;
@@ -19,10 +24,39 @@ public class Worker : BackgroundService
             string yamlContent = File.ReadAllText("./form_description.yaml");
             IFormFieldReader reader = new YamlFormFieldReader(yamlContent);
             var list = reader.ReadFormFields();
-            foreach (var item in list)
+
+            Document pdf = new Aspose.Pdf.Document();
+
+            Page page = pdf.Pages.Add();
+
+            int yPos = 500;
+            
+            foreach (var el in list)
             {
-                Console.WriteLine($"type {item.Type} name {item.Name} label {item.Label}");
+                if (el.Type == "fieldText")
+                {
+                    TextBoxField textBoxField = new TextBoxField(page, new Aspose.Pdf.Rectangle(100, yPos, 300, yPos+40));
+                    textBoxField.PartialName = "textbox" + yPos/100;
+                    textBoxField.Value = el.Name;
+                    
+
+                    Border border = new Border(textBoxField);
+                    border.Width = 5;
+                    border.Style = BorderStyle.Solid;
+                    textBoxField.Border = border;
+
+                    textBoxField.Color = Aspose.Pdf.Color.Black;
+                    
+                    pdf.Form.Add(textBoxField, 1);                    
+                }
+                yPos -= 100;
             }
+            
+            TextFragment textFragment = new TextFragment("Enter here name");
+            textFragment.Position = new Position(100, yPos - 50);
+            page.Paragraphs.Add(textFragment);
+            
+            pdf.Save("письмо.pdf");
 
             break;
         }
