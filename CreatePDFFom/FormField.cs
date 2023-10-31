a70bd545f10e71e342039c92b85219f398ce9b41
victorzv <kaplantigra@gmail.com>
2023-10-31 21:04:59 +0200

    new format

--- CreatePDFFom/FormField.cs
+++ CreatePDFFom/FormField.cs
@@ -5,6 +5,4 @@ public class FormField
     public string Type { get; set; }
     public string Name { get; set; }
     public string Label { get; set; }
-    public string Font { get; set; }
-    public int FontSize { get; set; }
 }
